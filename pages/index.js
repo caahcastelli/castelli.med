a7111a660e21814afc1b294eed4106737a99f467
Alexandre Crespo <xandycrespo@hotmail.com>
2021-11-21 18:47:53 -0300

    Página versão 4

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import imgcastelli from '../public/castelli.png'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,7 +9,6 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Castelli.med</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
@@ -16,12 +16,11 @@ export default function Home() {
           Carolina Castelli
         </h1>
 
-        <p className={styles.description}>
-          A mais linda do mundo!
-        </p>
-
         <Image src={imgcastelli} width={200} height={200}></Image>
 
+        <Link href="http://eepurl.com/hGbpf9">
+          <a className={styles.button}> Inscreva-se e receba novos materiais </a>
+        </Link>
 
       </main>
 
